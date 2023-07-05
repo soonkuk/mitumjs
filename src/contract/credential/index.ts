fed3296de4ae135920a9179b2ac3a4f8475fa8fa
JustinInkyunPark <justin.park@socialinfratech.com>
2023-07-05 18:17:28 +0900

    fix: credential api

--- src/contract/credential/index.ts
+++ src/contract/credential/index.ts
@@ -1,11 +1,14 @@
 import { isIPAddress } from "../../utils/validation.js";
 import { TimeStamp } from "../../utils/time.js";
+import { isAddress } from "../../utils/validation.js";
+import { OperationType } from "../../types/operation.js";
+import { Fact } from "../../types/fact.js";
 
 export class Credential {
   private _networkID: string = "";
   private _node: string = "";
   private _address: string = "";
-  private _credential: string = "";
+  private _credentialID: string = "";
 
   constructor(networkID: string, provider?: string) {
     this._setNode(provider);
@@ -22,7 +25,33 @@ export class Credential {
     this._networkID = networkID;
   }
 
-  createService(): OperationType<Fact> {
+  setContractAddress(contractAddress: string) {
+    if (this._address !== contractAddress && isAddress(contractAddress)) {
+      this._address = contractAddress;
+      console.log("Contract address is changed : ", this._address);
+    } else {
+      console.error("This is invalid address type");
+    }
+  }
+
+  setCredentialId(_credentialID: string) {
+    if (this._credentialID !== _credentialID) {
+      this._credentialID = _credentialID;
+      console.log("Collection ID is changed : ", this._credentialID);
+    } else {
+      console.error("This is invalid collection ID type");
+    }
+  }
+
+  getContractAddress(): string {
+    return this._address.toString();
+  }
+
+  getCredentialId(): string {
+    return this._credentialID.toString();
+  }
+
+  createCredential(): OperationType<Fact> {
     const token = new TimeStamp().UTC();
 
     return new OperationType(this._networkID, fact);
