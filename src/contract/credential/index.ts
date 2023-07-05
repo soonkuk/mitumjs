7b1a465725986c1f529618e268fe9186fd06d097
JustinInkyunPark <justin.park@socialinfratech.com>
2023-07-05 18:05:16 +0900

    feat: user's credential api

--- src/contract/credential/index.ts
+++ src/contract/credential/index.ts
@@ -1,10 +1,11 @@
 import { isIPAddress } from "../../utils/validation.js";
+import { TimeStamp } from "../../utils/time.js";
 
 export class Credential {
   private _networkID: string = "";
   private _node: string = "";
   private _address: string = "";
-  private _collection: string = "";
+  private _credential: string = "";
 
   constructor(networkID: string, provider?: string) {
     this._setNode(provider);
@@ -19,5 +20,29 @@ export class Credential {
 
   private _setChain(networkID: string) {
     this._networkID = networkID;
+  }
+
+  createService(): OperationType<Fact> {
+    const token = new TimeStamp().UTC();
+
+    return new OperationType(this._networkID, fact);
+  }
+
+  addTemplate(): OperationType<Fact> {
+    const token = new TimeStamp().UTC();
+
+    return new OperationType(this._networkID, fact);
+  }
+
+  issue(): OperationType<Fact> {
+    const token = new TimeStamp().UTC();
+
+    return new OperationType(this._networkID, fact);
+  }
+
+  revoke(): OperationType<Fact> {
+    const token = new TimeStamp().UTC();
+
+    return new OperationType(this._networkID, fact);
   }
 }
